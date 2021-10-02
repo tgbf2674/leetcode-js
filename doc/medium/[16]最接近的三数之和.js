27a3de4b7e13c63bb160102219c1d78e03eba6db
maqing <136518041@qq.com>
2021-10-03 01:53:26 +0800

    update

--- doc/medium/[16]最接近的三数之和.js
+++ doc/medium/[16]最接近的三数之和.js
@@ -29,16 +29,16 @@
  * @return {number}
  */
 var threeSumClosest = function(nums, target) {
-  nums.sort((a, b) => a - b)
-  let res = nums[0] + nums[1] + nums[nums.length - 1]
-  for (let i = 0; i < nums.length - 2; i++) {
-    let l = i + 1
-    let r = nums.length - 1
-    while (l < r) {
-      const sum = nums[i] + nums[l] + nums[r]
-      if (sum > target) r--
+  nums.sort((a,b)=> a-b)
+  let res = nums[0]+ nums[1] + nums[nums.length-1]
+  for(let i = 0;i<nums.length-2;i++){
+    let l = i+1
+    let r = nums.length-1
+    while (r>l){
+      let sum = nums[i]+nums[l]+nums[r]
+      if(sum>target)r--
       else l++
-      if (Math.abs(sum - target) < Math.abs(res - target)) {
+      if(Math.abs(sum-target)<Math.abs(res-target)){
         res = sum
       }
     }
@@ -46,3 +46,4 @@ var threeSumClosest = function(nums, target) {
   return res
 };
 //leetcode submit region end(Prohibit modification and deletion)
+
