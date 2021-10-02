8eff73d8143cd7e215bd2fd80f755c52d6c209e5
maqing <136518041@qq.com>
2021-10-02 23:57:57 +0800

    update

---
+++ [16]最接近的三数之和.js
@@ -0,0 +1,48 @@
+//给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和
+//。假定每组输入只存在唯一答案。
+//
+//
+//
+// 示例：
+//
+// 输入：nums = [-1,2,1,-4], target = 1
+//输出：2
+//解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2) 。
+//
+//
+//
+//
+// 提示：
+//
+//
+// 3 <= nums.length <= 10^3
+// -10^3 <= nums[i] <= 10^3
+// -10^4 <= target <= 10^4
+//
+// Related Topics 数组 双指针 排序 👍 897 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+  nums.sort((a, b) => a - b)
+  let res = nums[0] + nums[1] + nums[nums.length - 1]
+  for (let i = 0; i < nums.length - 2; i++) {
+    let l = i + 1
+    let r = nums.length - 1
+    while (l < r) {
+      const sum = nums[i] + nums[l] + nums[r]
+      if (sum > target) r--
+      else l++
+      if (Math.abs(sum - target) < Math.abs(res - target)) {
+        res = sum
+      }
+    }
+  }
+  return res
+};
+//leetcode submit region end(Prohibit modification and deletion)
--- doc/medium/[15]三数之和.js
+++ doc/medium/[15]三数之和.js
@@ -68,27 +68,3 @@ var threeSum = function(nums) {
   return res
 };
 //leetcode submit region end(Prohibit modification and deletion)
-
-let res = []
-let length = nums.length
-if(nums === null || length<3)return res
-nums.sort((a,b)=> a-b)
-for(let i = 0;i<nums.length;i++){
-  if(nums[i]>0)break
-  if(i>0 && nums[i] === nums[i-1])continue
-  let l = i+1
-  let r = length-1
-  while(l<r){
-    const sum = nums[i]+nums[r]+nums[l]
-    if(sum === 0){
-      res.push([nums[i],nums[l],nums[r]])
-      while (l<r && nums[l] === nums[l+1])l++
-      while (l<r && nums[r] === nums[r-1])r--
-      l++
-      r--
-    }
-    else if(sum<0)l++
-    else if(sum>0)r--
-  }
-}
-return res
