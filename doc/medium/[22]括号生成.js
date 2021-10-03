c0d65c432cef7e95117ad6dabbf7795c3781c17d
maqing <136518041@qq.com>
2021-10-03 23:25:02 +0800

    update

---
+++ doc/medium/[22]括号生成.js
@@ -0,0 +1,51 @@
+//数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+//
+// 有效括号组合需满足：左括号必须以正确的顺序闭合。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：n = 3
+//输出：["((()))","(()())","(())()","()(())","()()()"]
+//
+//
+// 示例 2：
+//
+//
+//输入：n = 1
+//输出：["()"]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= n <= 8
+//
+// Related Topics 字符串 动态规划 回溯 👍 2070 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+  const res = []
+  const dfs = (l,r,str)=>{
+    if(str.length === 2*n){
+      res.push(str)
+      return
+    }
+    if(l>0){
+      dfs(l-1,r,str+"(")
+    }
+    if(l<r){
+      dfs(l,r-1,str+")")
+    }
+  }
+};
+//leetcode submit region end(Prohibit modification and deletion)
