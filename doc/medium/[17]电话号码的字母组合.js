13f694b8a74ca6fa394d79c5e39f43648ed16afe
maqing <136518041@qq.com>
2021-10-03 12:32:17 +0800

    update

---
+++ doc/medium/[17]电话号码的字母组合.js
@@ -0,0 +1,63 @@
+//给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+//
+// 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+//
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：digits = "23"
+//输出：["ad","ae","af","bd","be","bf","cd","ce","cf"]
+//
+//
+// 示例 2：
+//
+//
+//输入：digits = ""
+//输出：[]
+//
+//
+// 示例 3：
+//
+//
+//输入：digits = "2"
+//输出：["a","b","c"]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 0 <= digits.length <= 4
+// digits[i] 是范围 ['2', '9'] 的一个数字。
+//
+// Related Topics 哈希表 字符串 回溯 👍 1528 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+  if(digits.length===0)return []
+  const res = []
+  const map = { '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz' }
+  const dfs = (cur,i)=>{
+    if(i>digits.length-1){
+      res.push(cur)
+      return
+    }
+    let letters = map[digits[i]]
+    for(const letter of letters){
+      dfs(cur+letter,i+1)
+    }
+  }
+  dfs('',0)
+  return res
+};
+//leetcode submit region end(Prohibit modification and deletion)
