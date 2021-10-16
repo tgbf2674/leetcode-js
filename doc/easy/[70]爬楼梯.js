20b7d3568bea056844a2b2b9fbb710c689a88367
maqing <136518041@qq.com>
2021-10-17 00:27:19 +0800

    update

--- doc/easy/[70]爬楼梯.js
+++ doc/easy/[70]爬楼梯.js
@@ -1,18 +1,18 @@
-//假设你正在爬楼梯。需要 n 阶你才能到达楼顶。 
+//假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
 //
-// 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？ 
+// 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
 //
-// 注意：给定 n 是一个正整数。 
+// 注意：给定 n 是一个正整数。
 //
-// 示例 1： 
+// 示例 1：
 //
 // 输入： 2
 //输出： 2
 //解释： 有两种方法可以爬到楼顶。
 //1.  1 阶 + 1 阶
-//2.  2 阶 
+//2.  2 阶
 //
-// 示例 2： 
+// 示例 2：
 //
 // 输入： 3
 //输出： 3
@@ -20,7 +20,7 @@
 //1.  1 阶 + 1 阶 + 1 阶
 //2.  1 阶 + 2 阶
 //3.  2 阶 + 1 阶
-// 
+//
 // Related Topics 记忆化搜索 数学 动态规划 👍 1935 👎 0
 
 
@@ -30,6 +30,18 @@
  * @return {number}
  */
 var climbStairs = function(n) {
+  // let res = [1,1]
+  // for (let i = 2;i<=n;i++){
+  //   res[i] = res[i-1]+ res[i-2]
+  // }
+  // return res[n]
 
+  let pre = 1,preOfpre = 1,cur = 1
+  for (let i =2;i<=n;i++){
+    cur = pre+ preOfpre
+    preOfpre = pre
+    pre = cur
+  }
+  return cur
 };
 //leetcode submit region end(Prohibit modification and deletion)
