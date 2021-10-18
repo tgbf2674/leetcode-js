774d485472af531fe5072cb293c2f57a7970fd59
maqing <136518041@qq.com>
2021-10-19 00:27:19 +0800

    update

--- doc/easy/[88]合并两个有序数组.js
+++ doc/easy/[88]合并两个有序数组.js
@@ -62,16 +62,14 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-  let len1 = m - 1;
-  let len2 = n - 1;
-  let len = m + n - 1;
-  while(len1 >= 0 && len2 >= 0) {
-    nums1[len--] = nums1[len1] > nums2[len2] ? nums1[len1--] : nums2[len2--];
+  let i = m-1,j = n-1,len = m+n-1
+  while (i>= 0 || j>=0){
+    if(i<0)nums1[len--] = nums2[j--]
+    else if (j<0)nums1[len--] = nums1[i--]
+    else if(nums1[i]<nums2[j])nums1[len--] = nums2[j--]
+    else nums1[len--] = nums1[i--]
   }
-  function arrayCopy(src, srcIndex, dest, destIndex, length) {
-    dest.splice(destIndex, length, ...src.slice(srcIndex, srcIndex + length));
-  }
-  arrayCopy(nums2, 0, nums1, 0, len2 + 1);
-
+  return nums1
 };
 //leetcode submit region end(Prohibit modification and deletion)
+
