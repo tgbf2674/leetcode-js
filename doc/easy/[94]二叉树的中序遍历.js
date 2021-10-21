ca64c0d82312708a1a1fc313c5b4b816ebf48aab
maqing <136518041@qq.com>
2021-10-22 01:09:41 +0800

    update

---
+++ doc/easy/[94]二叉树的中序遍历.js
@@ -0,0 +1,79 @@
+//给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：root = [1,null,2,3]
+//输出：[1,3,2]
+//
+//
+// 示例 2：
+//
+//
+//输入：root = []
+//输出：[]
+//
+//
+// 示例 3：
+//
+//
+//输入：root = [1]
+//输出：[1]
+//
+//
+// 示例 4：
+//
+//
+//输入：root = [1,2]
+//输出：[2,1]
+//
+//
+// 示例 5：
+//
+//
+//输入：root = [1,null,2]
+//输出：[1,2]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点数目在范围 [0, 100] 内
+// -100 <= Node.val <= 100
+//
+//
+//
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+// Related Topics 栈 树 深度优先搜索 二叉树 👍 1140 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+  let res= []
+  const inorder = (root)=>{
+    if(!root)return root
+    inorder(root.left)
+    res.push(root.val)
+    inorder(root.right)
+  }
+  inorder(root)
+  return res
+};
+//leetcode submit region end(Prohibit modification and deletion)
