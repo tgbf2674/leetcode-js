94ce44ac7882cb1801ecf58ff6d69645a9b181ef
maqing <136518041@qq.com>
2021-10-22 01:03:36 +0800

    update

--- doc/medium/[91]解码方法.js
+++ doc/medium/[91]解码方法.js
@@ -71,19 +71,29 @@
  * @param {string} s
  * @return {number}
  */
-var numDecodings = function(s) {
-  const n  =s.length
-  const dp = new Array(n+1).fill(0)
-  dp[0]=1
-  for (let i = 1;i<=n;i++){
-    if(s[i-1]!=='0'){
-      dp[i] += dp[i-1]
-    }
-    if (i> 1 && s[i-2]=== '1' || s[i-2] === '2' && parseInt(s[i-1])>=0 && parseInt(s[i-1])<=6){
-      dp[i] += dp[i-2]
-    }
+var numDecodings = function (s) {
+  // const n  =s.length
+  // const dp = new Array(n+1).fill(0)
+  // dp[0]=1
+  // for (let i = 1;i<=n;i++){
+  //   if(s[i-1]!=='0'){
+  //     dp[i] += dp[i-1]
+  //   }
+  //   if (i> 1 && s[i-2]=== '1' || s[i-2] === '2' && parseInt(s[i-1])>=0 && parseInt(s[i-1])<=6){
+  //     dp[i] += dp[i-2]
+  //   }
+  // }
+  // return dp[n]
+  const n = s.length
+  let a= 1, b= 1,c =0
+  for (let i =1;i<=n;i++){
+    c=0
+    if(s[i-1]!== '0') c+= b
+    if(s[i-2] === '1' || s[i-2] === '2' && parseInt(s[i-1])>=0 && parseInt(s[i-1])<= 6) c+=a
+    a=b
+    b=c
   }
-  return dp[n]
-};
+  return c
+}
 //leetcode submit region end(Prohibit modification and deletion)
 
