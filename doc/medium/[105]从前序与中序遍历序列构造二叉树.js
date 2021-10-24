c3127fad478285d22a12aab50f8a13f5c26f44a5
maqing <136518041@qq.com>
2021-10-25 00:54:41 +0800

    update

---
+++ doc/medium/[105]从前序与中序遍历序列构造二叉树.js
@@ -0,0 +1,61 @@
+//给定一棵树的前序遍历 preorder 与中序遍历 inorder。请构造二叉树并返回其根节点。
+//
+//
+//
+// 示例 1:
+//
+//
+//Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+//Output: [3,9,20,null,null,15,7]
+//
+//
+// 示例 2:
+//
+//
+//Input: preorder = [-1], inorder = [-1]
+//Output: [-1]
+//
+//
+//
+//
+// 提示:
+//
+//
+// 1 <= preorder.length <= 3000
+// inorder.length == preorder.length
+// -3000 <= preorder[i], inorder[i] <= 3000
+// preorder 和 inorder 均无重复元素
+// inorder 均出现在 preorder
+// preorder 保证为二叉树的前序遍历序列
+// inorder 保证为二叉树的中序遍历序列
+//
+// Related Topics 树 数组 哈希表 分治 二叉树 👍 1270 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function(preorder, inorder) {
+  if(preorder.length=== 0)return null
+  let root = new TreeNode(preorder[0])
+  let mid = inorder.indexOf(preorder[0])
+  root.left = buildTree(preorder.slice(1,mid+1),inorder.slice(0,mid))
+  root.right = buildTree(preorder.slice(mid+1),inorder.slice(mid+1))
+  return root
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+
+
