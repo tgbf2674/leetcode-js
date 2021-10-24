9c97d738cc03292920b49b14f82a2f31d274634a
maqing <136518041@qq.com>
2021-10-24 22:13:48 +0800

    update

---
+++ doc/easy/[101]对称二叉树.js
@@ -0,0 +1,57 @@
+//给定一个二叉树，检查它是否是镜像对称的。
+//
+//
+//
+// 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+//
+//     1
+//   / \
+//  2   2
+// / \ / \
+//3  4 4  3
+//
+//
+//
+//
+// 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+//
+//     1
+//   / \
+//  2   2
+//   \   \
+//   3    3
+//
+//
+//
+//
+// 进阶：
+//
+// 你可以运用递归和迭代两种方法解决这个问题吗？
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 1580 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+  if(!root)return true
+  const check=(left,right)=>{
+    if(!left && !right)return true
+    if(left && right){
+      return left.val === right.val && check(left.left,right.right) && check(left.right,right.left)
+    }
+    return false
+  }
+  return check(root.left,root.right)
+};
+//leetcode submit region end(Prohibit modification and deletion)
