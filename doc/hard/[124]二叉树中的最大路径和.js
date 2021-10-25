cf731421693ff4a850460513a533a953f5f9312b
maqing <136518041@qq.com>
2021-10-26 01:00:29 +0800

    update

---
+++ doc/hard/[124]二叉树中的最大路径和.js
@@ -0,0 +1,63 @@
+//路径 被定义为一条从树中任意节点出发，沿父节点-子节点连接，达到任意节点的序列。同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含一个 节点，且不
+//一定经过根节点。
+//
+// 路径和 是路径中各节点值的总和。
+//
+// 给你一个二叉树的根节点 root ，返回其 最大路径和 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：root = [1,2,3]
+//输出：6
+//解释：最优路径是 2 -> 1 -> 3 ，路径和为 2 + 1 + 3 = 6
+//
+// 示例 2：
+//
+//
+//输入：root = [-10,9,20,null,null,15,7]
+//输出：42
+//解释：最优路径是 15 -> 20 -> 7 ，路径和为 15 + 20 + 7 = 42
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点数目范围是 [1, 3 * 10⁴]
+// -1000 <= Node.val <= 1000
+//
+// Related Topics 树 深度优先搜索 动态规划 二叉树 👍 1259 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxPathSum = function(root) {
+  let maxSum = Number.MIN_SAFE_INTEGER
+  const dfs = (root)=>{
+    if(!root)return 0
+    const left = dfs(root.left)
+    const right = dfs(root.right)
+    let innerMaxSum = root.val + left+right
+    maxSum = Math.max(maxSum,innerMaxSum)
+    let outputMaxSum = root.val + Math.max(0,left,right)
+    return outputMaxSum< 0? 0: outputMaxSum
+  }
+  dfs(root)
+  return maxSum
+};
+//leetcode submit region end(Prohibit modification and deletion)
--- doc/medium/[122]买卖股票的最佳时机 II.js
+++ doc/medium/[122]买卖股票的最佳时机 II.js
@@ -70,12 +70,10 @@ var maxProfit = function(prices) {
 
 //  状态压缩
   const n = prices.length
-  let dp0 = 0,dp1=-prices[0]
-  for (let i =1;i<n;i++){
-    let newDp0 = Math.max(dp0,dp1+prices[i])
-    let newDp1 = Math.max(dp1,dp0-prices[i])
-    dp0 = newDp0
-    dp1 = newDp1
+  let dp0 = 0,dp1= -prices[0]
+  for (let i =1 ;i<n;i++){
+    dp0 = Math.max(dp0,dp1+prices[i])
+    dp1 = Math.max(dp1,dp0-prices[i])
   }
   return dp0
 };
